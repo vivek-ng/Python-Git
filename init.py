fd50ce145b2fccc3834eb7ba9284a2a31070aad3
vivek3894 <viveknagaraju@yahoo.in>
2018-12-22 20:55:17 +0530

    minor changes and refactoring

--- init.py
+++ init.py
@@ -2,9 +2,5 @@
 
 import sys
 
-# comm_length = len(sys.argv)
-# if comm_length < 2:
-# 	print("wrong usgage need to pass a command to work!")
-
 for args in sys.argv[1:]:
     print(args)
--- simple-git.py
+++ simple-git.py
@@ -7,6 +7,7 @@ import subprocess
 comm_length = len(sys.argv)
 if comm_length < 2:
 	print("wrong usgage need to pass a command to work!")
+	sys.exit()
 
 exec_options = sys.argv[1:]
 subprocess.call(exec_options)
